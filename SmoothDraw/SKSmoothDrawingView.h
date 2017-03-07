67eb00ac286a09ef0c5870c5fed45ecf9ee10485
sunshuaikun <sunshuaikun@gmail.com>
2017-03-07 10:53:16 +0800

    init commit

---
+++ SmoothDraw/SKSmoothDrawingView.h
@@ -0,0 +1,14 @@
+//
+//  SKSmoothDrawingView.h
+//  SmoothDraw
+//
+//  Created by sunshuaikun on 17/3/3.
+//  Copyright © 2017年 sunshuaikun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKSmoothDrawingView : UIView
+@property (nonatomic,strong)UIImageView    *drawImageView;
+- (void)reset;
+@end
